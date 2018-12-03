cf8cf77b3ce1c0cc1eeefc879ce1918e9f3e8888
Ludovic Cyril Michel <ludovic@tenatek.com>
2018-12-03 03:09:51 -0600

    fixed redundancy of calculations

--- gpu_nodisplay.cu
+++ gpu_nodisplay.cu
@@ -10,7 +10,7 @@ __global__ void calculate_acceleration(Vertex *v, unsigned int n) {
   unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
   unsigned int j = blockIdx.y * blockDim.y + threadIdx.y;
 
-  if (i < n && j < n && i != j) {
+  if (i < n && j < n && i < j) {
     float distance = sqrt(pow(v[i].position.x - v[j].position.x, 2) +
                           pow(v[i].position.y - v[j].position.y, 2) +
                           pow(v[i].position.z - v[j].position.z, 2));
