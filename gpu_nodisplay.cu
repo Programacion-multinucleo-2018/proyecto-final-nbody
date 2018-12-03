00329544e16df104d3bfb36e944f7c7dcf0218e1
Ludovic Cyril Michel <ludovic@tenatek.com>
2018-12-03 03:15:54 -0600

    fix

--- gpu_nodisplay.cu
+++ gpu_nodisplay.cu
@@ -107,7 +107,7 @@ int main(int argc, const char **argv) {
   numBlocks.y = 1;
   numThreads.y = 1;
   calculate_position<<<numBlocks, numThreads>>>(d_v, n_vertices, delta);
-  numBlocks.y = (int)ceil((float)n_vertices / 16.0;
+  numBlocks.y = (int)ceil((float)n_vertices / 16.0);
   numThreads.y = 16;
   calculate_acceleration<<<numBlocks, numThreads>>>(d_v, n_vertices);
   numBlocks.y = 1;
