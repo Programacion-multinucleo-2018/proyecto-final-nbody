ef4a2a5784d40b7461bf6058e83eccae1197f019
Ludovic Cyril Michel <ludovic@tenatek.com>
2018-12-03 15:14:50 -0600

    more progress

--- cpu.cc
+++ cpu.cc
@@ -70,9 +70,8 @@ void display(void) {
 
   glDisable(GL_DEPTH_TEST); // also disable the depth test so renders on top
 
-  glRasterPos2f(-0.7, -0.9);
+  glRasterPos2f(-1.0, -1.0);
   cout << -(float)width / 2.0 << " " << -(float)height / 2.0 << endl;
-  glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
   char buf[300];
   sprintf(buf, "Test");
   const char *p = buf;
